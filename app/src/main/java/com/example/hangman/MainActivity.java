8ffcacd293da39c80d1926ede87139e5b96f4998
Christian Andersen <angercraft@gmail.com>
2018-10-26 20:42:15 +0200

    Updated stuff.

--- app/src/main/java/com/example/hangman/MainActivity.java
+++ app/src/main/java/com/example/hangman/MainActivity.java
@@ -6,15 +6,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.example.hangman.game.GameActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    
+
     ImageButton play, highscore, settings, about;
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.hangmanlogo);
+
+        GlideApp.with(this).load(R.drawable.logoanimated).into(logo);
 
         play = findViewById(R.id.playbtn);
         highscore = findViewById(R.id.highscorebtn);
